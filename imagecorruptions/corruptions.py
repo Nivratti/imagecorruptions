1e404ec49232f3aacd0d42a64ad867ae1e108710
Nivratti <boyanenivratti@gmail.com>
2023-07-03 12:46:34 +0530

    fixed issue -- gaussian() got an unexpected keyword argument 'multichannel'

--- imagecorruptions/corruptions.py
+++ imagecorruptions/corruptions.py
@@ -200,7 +200,10 @@ def speckle_noise(x, severity=1):
 def gaussian_blur(x, severity=1):
     c = [1, 2, 3, 4, 6][severity - 1]
 
-    x = gaussian(np.array(x) / 255., sigma=c, multichannel=True)
+    try:
+        x = gaussian(np.array(x) / 255., sigma=c) # remove multichannel param for laatest skimage
+    except Exception as e:
+        x = gaussian(np.array(x) / 255., sigma=c, multichannel=True)
     return np.clip(x, 0, 1) * 255
 
 
